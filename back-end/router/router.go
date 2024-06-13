ccdac94afa0cea6c5cb970753156eab64069ddcb
石毓安 <shiyuan@shiyuandeMacBook-Air.local>
2024-06-14 01:51:28 +0800

    Add swagger api document

--- back-end/router/router.go
+++ back-end/router/router.go
@@ -3,9 +3,12 @@ package router
 import (
 	"fmt"
 	"os"
+	docs "webserver/docs"
 	v1 "webserver/router/api/v1"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Init() {
@@ -16,12 +19,15 @@ func Init() {
 	// router.GET("/", func(c *gin.Context) {
 	// 	c.String(http.StatusOK, "Hello")
 	// })
-
+	docs.SwaggerInfo.BasePath = "/api/v1"
 	apiV1 := router.Group("/v1")
 	{
 		apiV1.POST("/login", v1.Login)
 		apiV1.POST("/register", v1.Register)
+
 	}
+	//start swagger page
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	webPort := fmt.Sprintf("0.0.0.0:%s", port)
 	router.Run(webPort)
 }
